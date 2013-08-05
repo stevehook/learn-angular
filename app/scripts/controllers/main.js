657f739680422727795734bce44fb563f3c971d5
Steve <steve.hook@gmail.com>
2013-08-05 19:16:45 +0100

    Run Yeoman to bootstrap angular and other libraries

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('learnAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
